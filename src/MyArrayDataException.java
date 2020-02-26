34f11864817fb57f15d74f4c825ccdca0f5d1d0a
Elvira Kutuzova <elvira.kutuzova@gmail.com>
2020-02-26 22:23:30 +0400

    Initial commit

---
+++ src/MyArrayDataException.java
@@ -0,0 +1,5 @@
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException() {super("Элемент массива не возможно преобразовать в числовое значение!");
+
+    }
+}
---
+++ src/MyArraySizeException.java
@@ -0,0 +1,7 @@
+public class MyArraySizeException extends RuntimeException {
+
+    public MyArraySizeException() {
+       super("Массив слишком велик! Необходим массив размером 4x4.");
+    }
+
+}
